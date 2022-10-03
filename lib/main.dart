17212375fb6bc8b25ed1219d37b4cfa060315253
Саят Бектенбай <sayat.b@a2data.ai>
2022-10-03 14:25:48 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,105 +11,145 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Weather',
+            style: TextStyle(color: Colors.black54),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: const IconButton(onPressed: null, icon: Icon(Icons.menu)),
+          actions: const [
+            IconButton(onPressed: null, icon: Icon(Icons.settings))
           ],
         ),
+        body: _buildBody(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        _headerImage(),
+        SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _weatherDescription(),
+                  const Divider(),
+                  _temperature(),
+                  const Divider(),
+                  _temperatureForecast(),
+                  const Divider(),
+                  _footerRatings()
+                ],
+              ),
+            )
+        ),
+      ],
+    ),
+  );
+}
+
+Image _headerImage() {
+  return const Image(
+    image: NetworkImage(
+        'https://media.istockphoto.com/photos/weather-forecast-concept-picture-id531889697?b=1&k=20&m=531889697&s=170667a&w=0&h=imZl8VcPhgj-6jTFFkYGbVTFOTyQ-u93Z9WsyhMdqps='),
+    fit: BoxFit.cover,
+  );
+}
+
+Column _weatherDescription() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: const [
+      Text(
+        'Tuesday - May 22',
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+      Divider(),
+      Text(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sodales, mi ut pulvinar laoreet, orci magna lacinia neque, in semper libero sem ut massa. Donec vel tortor orci. Praesent vulputate sem ac finibus posuere. Duis ut sem finibus, hendrerit erat non, iaculis eros.',
+        style: TextStyle(color: Colors.black54),
+      )
+    ],
+  );
+}
+
+Row _temperature() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      const SizedBox(width: 16,),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: const [
+              Text('15* Clear', style: TextStyle(color: Colors.deepPurple),)
+            ],
+          ),
+          Row(
+            children: const [
+              Text('Almaty oblysy, Almaty', style: TextStyle(color: Colors.grey),)
+            ],
+          )
+        ],
+      )
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10,
+    children: List.generate(7, (int index) {
+      return Chip(
+        label: Text('${index + 20} *C', style: const TextStyle(fontSize: 15),),
+        avatar: const Icon(Icons.cloud, color: Colors.lightBlue,),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+          side: const BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      );
+    }),
+  );
+}
+
+Row _footerRatings() {
+  var stars = Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Icon(Icons.star, size: 15, color: Colors.yellow[600],),
+      Icon(Icons.star, size: 15, color: Colors.yellow[600],),
+      Icon(Icons.star, size: 15, color: Colors.yellow[600],),
+      const Icon(Icons.star, size: 15, color: Colors.black,),
+      const Icon(Icons.star, size: 15, color: Colors.black,),
+    ],
+  );
+
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      const Text('Info with openweathermap.org', style: TextStyle(fontSize: 15),),
+      stars
+    ],
+  );
 }
